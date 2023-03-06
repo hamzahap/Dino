1c07059d2ec798623d31045fb012ee28422ea2dc
Hamzah Punjabi <hamzahqatar123@gmail.com>
2023-03-06 02:33:38 -0330

    Tensorflow I no understand, fix to pyTorch, I still dont understand

--- Dino.py
+++ Dino.py
@@ -1,283 +1,209 @@
-import numpy as np
 import pygame
-import sys
 import random
-import time
-
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
-
-# Set up the game parameters
-WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 150
-GROUND_HEIGHT = 125
-OBSTACLE_WIDTH = 20
-OBSTACLE_COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255), (255, 0, 255)]
-OBSTACLE_HEIGHTS = [1, 2, 3, 4, 5]
-OBSTACLE_SPAWN_RATE = 100
-OBSTACLE_SPEED = 5
-DINO_SPEED = 8
-JUMP_VELOCITY = 18
-GRAVITY = 1
-FONT_SIZE = 30
-MEMORY_CAPACITY = 100000
-BATCH_SIZE = 32
-DISCOUNT_FACTOR = 0.99
-LEARNING_RATE = 0.1
-INITIAL_EPSILON = 1.0
-EPSILON_MIN = 0.01
-EPSILON_DECAY = 0.99
-NUM_ACTIONS = 2
-STATE_SIZE = 4
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-EPISODES = 1000
-NUM_EPISODES = 100
-
-
-# Initialize pygame
-pygame.init()
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 # Set up the game window
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 400
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Dino Game")
 
-# Set up the font
-FONT = pygame.font.SysFont("Comic Sans MS", FONT_SIZE)
+# Set up game variables
+GROUND_HEIGHT = 350
+dino = pygame.Rect(50, GROUND_HEIGHT - 40, 40, 40)
+obstacles = []
+obstacle_speed = 5
+obstacle_height_scale = 1
+score = 0
 
-# Define the Dino class
-class Dino:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.velocity = 0
-        self.width = 30
-        self.height = 50
+# Initialize the font system
+pygame.font.init()
 
-    def jump(self):
-        self.velocity = -JUMP_VELOCITY
+# Set up fonts
+FONT = pygame.font.SysFont(None, 48)
 
-    def update(self):
-        self.velocity += GRAVITY
-        self.y += self.velocity
+# Set up colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
-        if self.y > GROUND_HEIGHT:
-            self.y = GROUND_HEIGHT
-            self.velocity = 0
+# Define the neural network
+class DinoNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size + 1, hidden_size, bias=True)
+        self.fc2 = nn.Linear(hidden_size, output_size, bias=True)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
 
-    def draw(self, surface):
-        rect = pygame.Rect(self.x, self.y - self.height, self.width, self.height)
-        pygame.draw.rect(surface, (0, 0, 0), rect)
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return x
 
-# Define the Obstacle class
-class Obstacle:
-    def __init__(self, x, height):
-        self.x = x
-        self.y = GROUND_HEIGHT - OBSTACLE_WIDTH * height
-        self.width = OBSTACLE_WIDTH
-        self.height = OBSTACLE_WIDTH * height
-        self.color = random.choice(OBSTACLE_COLORS)
+# Set up the neural network
+input_size = 10 # the size of the input feature vector
+hidden_size = 64 # the size of the hidden layer in the neural network
+output_size = 1 # the size of the output (i.e. the probability of jumping)
+model = DinoNet(input_size, hidden_size, output_size)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    def update(self, speed):
-        self.x -= speed
+# Define the replay buffer
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
 
-    def draw(self, surface):
-        rect = pygame.Rect(self.x, self.y, self.width, self.height)
-        pygame.draw.rect(surface, self.color, rect)
+    def push(self, state, action, reward, next_state, done):
+        experience = (state, action, reward, next_state, done)
+        self.buffer.append(experience)
+        if len(self.buffer) > self.capacity:
+            self.buffer.pop(0)
 
-# Spawn a new obstacle
-def spawn_obstacle(obstacles):
-    height = random.choice(OBSTACLE_HEIGHTS)
-    obstacle = Obstacle(WINDOW_WIDTH, height)
-    obstacles.append(obstacle)
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = map(np.stack, zip(*batch))
+        return state, action, reward, next_state, done
 
-# Update the positions of the obstacles
-def update_obstacles(obstacles, speed):
-    for obstacle in obstacles:
-        obstacle.update(speed)
+    def __len__(self):
+        return len(self.buffer)
 
-# Remove any obstacles that are off the screen
-def remove_offscreen_obstacles(obstacles):
-    obstacles[:] = [obstacle for obstacle in obstacles if obstacle.x + obstacle.width > 0]
+# Set up the replay buffer
+capacity = 100000 # the maximum capacity of the replay buffer
+batch_size = 32 # the size of the batches used for training
+replay_buffer = ReplayBuffer(capacity)
 
-# Function to draw score to the screen
-def draw_score(score, episode, epsilon, time):
-    font = pygame.font.Font(None, 36)
-    text = font.render("Score: " + str(score) + " Episode: " + str(episode) + " Epsilon: " + str(round(epsilon, 2)) + " Time: " + str(round(time, 2)), True, BLACK)
-    text_rect = text.get_rect()
-    text_rect.centerx = WINDOW_WIDTH // 2
-    text_rect.centery = 20
-    WINDOW.blit(text, text_rect)
+# Define the hyperparameters
+gamma = 0.99 # the discount factor for future rewards
+epsilon = 1.0 # the probability of taking a random action
+epsilon_min = 0.01 # the minimum value of epsilon
+epsilon_decay = 0.999 # the rate at which epsilon decays over time
 
-# Check for collisions between the dino and the obstacles
-def check_collisions(dino, obstacles):
-    for obstacle in obstacles:
-        obstacle_height = obstacle.height // OBSTACLE_WIDTH
-        if dino.y + dino.height > obstacle.y and dino.x + dino.width > obstacle.x and dino.x < obstacle.x + obstacle.width:
-            return True
-    return False
-
-# Choose an action using the SARSA algorithm
-def choose_action(model, state, epsilon):
-    if np.random.random() < epsilon:
-        return np.random.choice(NUM_ACTIONS)
-    else:
-        q_values = model.predict(np.expand_dims(state, axis=0))
-        return np.argmax(q_values[0])
-
-
-# Update the Q-learning model using the SARSA algorithm
-def replay(memory, model, batch_size, discount_factor, learning_rate):
-    if len(memory) < batch_size:
-        return
-
-    batch = random.sample(memory, batch_size)
-    states = np.array([item[0] for item in batch])
-    actions = np.array([item[1] for item in batch])
-    rewards = np.array([item[2] for item in batch])
-    next_states = np.array([item[3] for item in batch])
-    next_actions = np.array([choose_action(model, state, epsilon=0) for state in next_states])
-    q_values = model.predict(states)
-    next_q_values = model.predict(next_states)
-    targets = rewards + discount_factor * next_q_values[np.arange(batch_size), next_actions]
-    q_values[np.arange(batch_size), actions] = (1 - learning_rate) * q_values[np.arange(batch_size), actions] + learning_rate * targets
-    model.fit(states, q_values, verbose=0)
-
-# Train the Q-learning model using SARSA
-def train(model, memory, batch_size, discount_factor, learning_rate, epsilon, epsilon_min, epsilon_decay, obstacles):
-    state = np.array([dino.y, (dino.y - dino.velocity) / 10, obstacles[0].x, obstacles[0].height])
-    total_reward = 0
-    done = False
-    score_list = []
-    time_list = []
-    for episode in range(EPISODES):
-        while not done:
-            action = choose_action(model, state, epsilon)
-            dino.jump() if action == 1 else None
-            dino.update()
-            update_obstacles(obstacles, OBSTACLE_SPEED)
-            remove_offscreen_obstacles(obstacles)
-            if len(obstacles) == 0:
-                spawn_obstacle(obstacles)
-            if check_collisions(dino, obstacles):
-                reward = -10
-                done = True
-            else:
-                reward = 1
-            next_state = np.array([dino.y, (dino.y - dino.velocity) / 10, obstacles[0].x, obstacles[0].height]) if len(obstacles) > 0 else state
-            memory.append((state, action, reward, next_state))
-            replay(memory, model, batch_size, discount_factor, learning_rate)
-            state = next_state
-            total_reward += reward
-        epsilon = max(epsilon * EPSILON_DECAY, EPSILON_MIN)
-        score_list.append(total_reward)
-        time_list.append(pygame.time.get_ticks() / 1000)
-        if episode % 10 == 0:
-            avg_score = sum(score_list) / len(score_list)
-            avg_time = sum(time_list) / len(time_list)
-            print("Episode:", episode, "  Memory size:", len(memory), "  Epsilon:", round(epsilon, 2), "  Average score:", round(avg_score, 2), "  Average time:", round(avg_time, 2))
-            score_list = []
-            time_list = []
-        total_reward = 0
-        done = False
-        obstacles.clear()
-        spawn_obstacle(obstacles)
-        dino.x, dino.y = 50, 100
-        state = np.array([dino.y, (dino.y - dino.velocity) / 10, obstacles[0].x, obstacles[0].height])
-        draw_window(dino, obstacles, 0, episode, epsilon, 0)
-        for i in range(3):
-            spawn_obstacle(obstacles)
-        pygame.event.pump()
-
-
-    return total_reward
-
-# Run the game
-def run_game(model):
-    for episode in range(NUM_EPISODES):
-        obstacles = []
-        memory = []
-        global dino
-        dino = Dino(50, GROUND_HEIGHT)
-        epsilon = max(EPSILON_MIN, INITIAL_EPSILON * EPSILON_DECAY ** episode)
+# Set up the game loop
+clock = pygame.time.Clock()
+running = True
+episode = 0
+while running:
+    # Reset the game if the dinosaur collides with an obstacle
+    if any(obstacle.colliderect(dino) for obstacle in obstacles):
+        # Update the score and print the result
+        score_text = FONT.render("Score: " + str(score), True, BLACK)
+        print(f"Episode {episode}: score={score}")
+        episode += 1
         score = 0
-        start_time = time.time()
-        state = None  # Initialize state variable to None
+        obstacles = []
+        # Reset the dinosaur and obstacles
+        dino = pygame.Rect(50, GROUND_HEIGHT - 40, 40, 40)
+        obstacle_speed = 5
+        obstacle_height_scale = 1
 
-        while True:
-            if len(obstacles) == 0:
-                spawn_obstacle(obstacles)
-                state = np.array([dino.y, (dino.y - dino.velocity) / 10, obstacles[0].x, obstacles[0].height])
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-            # Handle events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+    # Get the state of the game
+    state = np.array([
+        dino.x, dino.y,
+        *[obstacle.x for obstacle in obstacles],
+        *[obstacle.y for obstacle in obstacles],
+        *[obstacle_speed for obstacle in obstacles],
+        score
+    ])
 
-            # Choose an action and update the game state
-            action = choose_action(model, state, epsilon)
-            dino.jump() if action == 1 else None
-            dino.update()
-            update_obstacles(obstacles, OBSTACLE_SPEED)
-            remove_offscreen_obstacles(obstacles)
-            if len(obstacles) == 0:
-                spawn_obstacle(obstacles)
-            if check_collisions(dino, obstacles):
-                reward = -10
-                done = True
-            else:
-                reward = 1
-                done = False
-            next_state = np.array([dino.y, (dino.y - dino.velocity) / 10, obstacles[0].x, obstacles[0].height]) if len(obstacles) > 0 else state
-            memory.append((state, action, reward, next_state))
-            state = next_state
-            score += reward
+    # Choose an action
+    if np.random.rand() < epsilon:
+        action = np.random.randint(2)
+    else:
+        with torch.no_grad():
+            state_tensor = torch.FloatTensor(state).unsqueeze(0)
+            action_tensor = model(state_tensor)
+            action = int(action_tensor.squeeze().numpy() > 0.5)
 
-            # Update the model
-            replay(memory, model, BATCH_SIZE, DISCOUNT_FACTOR, LEARNING_RATE)
+    # Perform the action and update the game state
+    if action == 1 and dino.y == GROUND_HEIGHT - 40:
+        dino.y -= 100
+        dino.y -= 150
 
-            # Draw the game
-            WINDOW.fill(WHITE)
-            dino.draw(WINDOW)
-            for obstacle in obstacles:
-                obstacle.draw(WINDOW)
-            draw_score(score, episode, epsilon, time.time() - start_time)
-            pygame.display.update()
+    # Increase game speed over time
+    if pygame.time.get_ticks() % 5000 == 0:
+        obstacle_speed += 1
+        obstacle_height_scale += 0.2
 
-            # Check if the game is over
-            if done:
-                print("Episode:", episode, "Score:", score, "Time:", time.time() - start_time, "Memory Size:", len(memory))
-                break
+    # Spawn new obstacles randomly
+    if len(obstacles) < 5 and random.random() < 0.01:
+        obstacle_height = random.randint(1, 3) * obstacle_height_scale
+        obstacle = pygame.Rect(WINDOW_WIDTH, GROUND_HEIGHT - obstacle_height * 15, 20, obstacle_height * 15)
+        obstacles.append(obstacle)
 
-            draw_window(dino, obstacles, score, episode, epsilon, time.time() - start_time)
-
-
-
-
-
-
-# Draw the game window
-def draw_window(dino, obstacles, score, episode, epsilon, time):
-    WINDOW.fill(WHITE)
-    dino.draw(WINDOW)
+    # Move obstacles and check for collisions with the dino
     for obstacle in obstacles:
-        obstacle.draw(WINDOW)
-    draw_score(score, episode, epsilon, time)
-    pygame.display.update()
+        obstacle.x -= 5
+        obstacle.x -= obstacle_speed
+        if dino.colliderect(obstacle):
+            reward = -1
+            done = True
+            replay_buffer.push(state, action, reward, None, done)
+            break
+        elif obstacle.right < 0:
+            obstacles.remove(obstacle)
+            score += 1
+            reward = 1
+            done = False
+            replay_buffer.push(state, action, reward, None, done)
 
-# Create the Q-learning model
-model = Sequential()
-model.add(Dense(64, input_shape=(STATE_SIZE,), activation="relu"))
-model.add(Dense(64, activation="relu"))
-model.add(Dense(NUM_ACTIONS, activation="linear"))
-model.compile(loss="mse", optimizer=Adam(lr=LEARNING_RATE))
+    # Move the dino back to the ground
+    if dino.y < GROUND_HEIGHT - 40 and dino.y >= GROUND_HEIGHT - 140:
+        dino.y += 5
+    elif dino.y < GROUND_HEIGHT - 140:
+        dino.y += 10
 
-# Run the game
-run_game(model)
+    # Get the next state of the game
+    next_state = np.array([
+        dino.x, dino.y,
+        *[obstacle.x for obstacle in obstacles],
+        *[obstacle.y for obstacle in obstacles],
+        *[obstacle_speed for obstacle in obstacles],
+        score
+    ])
 
-# Quit the game
+    # Update the Q-values using the Bellman equation
+    if len(replay_buffer) > batch_size:
+        state_batch, action_batch, reward_batch, next_state_batch, done_batch = replay_buffer.sample(batch_size)
+        state_batch = torch.FloatTensor(state_batch)
+        action_batch = torch.FloatTensor(action_batch)
+        reward_batch = torch.FloatTensor(reward_batch)
+        next_state_batch = torch.FloatTensor(next_state_batch)
+        done_batch = torch.FloatTensor(done_batch)
+        q_values = model(state_batch)
+        next_q_values = model(next_state_batch)
+        target_q_values = reward_batch + gamma * torch.max(next_q_values, dim=1)[0] * (1 - done_batch)
+        q_values = q_values.gather(1, action_batch.long().unsqueeze(1)).squeeze(1)
+        loss = criterion(q_values, target_q_values.detach())
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    # Update epsilon
+    epsilon = max(epsilon_min, epsilon_decay * epsilon)
+
+    # Draw the game window
+    WINDOW.fill(WHITE)
+    pygame.draw.rect(WINDOW, BLACK, pygame.Rect(0, GROUND_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT - GROUND_HEIGHT))
+    pygame.draw.rect(WINDOW, (255, 0, 0), dino)
+    for obstacle in obstacles:
+        pygame.draw.rect(WINDOW, (0, 0, 255), obstacle)
+    score_text = FONT.render("Score: " + str(score), True, BLACK)
+    WINDOW.blit(score_text, (10, 10))
+    pygame.display.flip()
+
+    # Set the game clock
+    clock.tick(60)
+
 pygame.quit()
-sys.exit()
+
